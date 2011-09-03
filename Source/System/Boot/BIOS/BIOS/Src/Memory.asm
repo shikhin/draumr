3af83ea6f338fbefdd0ec977c5c7d89809035d69
Shikhin Sethi <draumr.shikhin@gmail.com>
2011-09-03 11:23:53 +0530

    Well, added few more memory detection methods.

--- Source/System/Boot/BIOS/BIOS/Src/Memory.asm
+++ Source/System/Boot/BIOS/BIOS/Src/Memory.asm
@@ -25,7 +25,8 @@ MMapBuild:
 .Prepare:
     xchg bx, bx
     mov di, MMap
-    
+    xor bp, bp                        ; The number of entries in the MMap.
+
 .BDA:    
     ; Set the BDA entry as 0x400->0x500 of type 4, unusable RAM.
     mov dword [di + 0], 0x400
@@ -33,14 +34,24 @@ MMapBuild:
     mov dword [di + 16], 4
     mov dword [di + 20], 1
     add di, 24
+    inc bp
 
 .Video:
-    ; Set the video mapped memory, and other portions as 0xA0000->0xFFFFF of type 4, unusable RAM.
-    mov dword [di + 0], 0xA0000
-    mov dword [di + 8], 0x60000
+    ; Set the legacy BIOS ROM area, and other portions as 0xC0000->0xFFFFF of type 4, unusable RAM.
+    mov dword [di + 0], 0xC0000
+    mov dword [di + 8], 0x40000
     mov dword [di + 16], 4
     mov dword [di + 20], 1
     add di, 24
+    inc bp
+
+    ; And then the real BIOS rom, 0xFE000000->0xFFFFFFFF of type 4, unusable RAM.
+    mov dword [di + 0], 0xFE000000
+    mov dword [di + 8], 0x1FFFFFF 
+    mov dword [di + 16], 4
+    mov dword [di + 20], 1
+    add di, 24
+    inc bp
 
 .BootCode:
     ; Set the boot code, as 0x7C00->End of our file, of type 2, Boot Code.
@@ -56,11 +67,203 @@ MMapBuild:
     mov dword [di + 16], 2
     mov dword [di + 20], 1
     add di, 24
+    inc bp
 
 .E820:
+    jmp .CMOS
     call E820
     jnc .Clean                        ; If were able to build using E820, then clean the map.
+    
     clc                               ; Clear the carry flag.
+
+; Int 0x12 would be the first to use (to get the, um, "base memory data").
+.12:
+    xor eax, eax                      ; Clear out AX.
+    int 0x12                          ; Call Int 0x12 - I assume it always works, which it should - if it doesn't, well, then, I don't think this would happen. :-)
+    mov ecx, 1024
+    mul ecx                           ; Now we got the length of the base entry in EAX.
+
+    mov dword [di + 8], eax           ; Get the length of the entry.
+    mov dword [di + 16], 1
+    mov dword [di + 20], 1            ; Now that's free RAM - type 1.
+    add di, 24                        ; That was the 'free' RAM area - let's do the 'system' RAM area.
+    inc bp
+
+    mov dword [di + 0], eax           ; Now, it starts at EAX - end of previous entry.
+    mov ebx, 0xA0000
+
+    cmp eax, 0xA0000                  ; If the free RAM region is around above 0xA0000 - wrap it to 0xA0000 (and the mess would be cleaned later).
+    jb .Cont
+
+    mov eax, 0x9FFFF
+
+.Cont:
+    sub ebx, eax                      ; EBX now contains the length of this entry.
+
+    mov dword [di + 8], ebx
+    mov dword [di + 16], 4
+    mov dword [di + 20], 1
+    add di, 24                        ; Move to next entry.
+    inc bp
+
+    mov dword [di], 0xF00000          ; Make the 0xF00000 - 0x1000000 entry for the ISA memory hole.
+    mov dword [di + 8], 0x100000
+    mov dword [di + 16], 4
+    mov dword [di + 20], 1
+    add di, 24
+    inc bp
+ 
+; There is a bigger chance that E881 succeeds where E801 fails.
+.E881:
+    mov eax, 0xE881
+    call E8X1                         ; E8X1 simply works on whatever is in EAX.
+    jnc .Clean                        ; If carry wasn't set, clean the map.
+
+    clc                               ; Else, clear the carry flag, and try 0xE801.
+
+.E801:
+    mov eax, 0xE801                   ; Or try 0xE801 now.
+    call E8X1                         
+    jnc .Clean        
+
+; Well, this is really confusing - which one is more reliable? I assume 0x8A is more reliable than 0xDA88.
+.8A:
+    clc
+
+    ; Clear out EDX and EAX - since we'd need the higher word to be clear in any case.
+    xor edx, edx
+    xor eax, eax
+    
+    mov ah, 0x8A
+    int 0x15
+    jc .DA88
+
+    ; Now, that should move the lower word into the higher word.
+    shl edx, 16
+    ; And put that into EAX.
+    or eax, edx
+
+    ; Since it is length in 1KiB block, multiply it by 1024 to get the length in bytes.
+    mov ecx, 1024
+    mul ecx
+
+    mov dword [di], 0x100000
+    mov dword [di + 8], eax
+    ; That's free RAM - and a usable entry.
+    mov dword [di + 16], 1
+    mov dword [di + 20], 1
+    add di, 24
+
+    ; Increase the "we have that many entries" count.
+    inc bp
+    mov [MMapHeader.Entries], bp
+
+    jmp .Clean
+
+.DA88:
+    clc
+
+    ; Clear out ECX and EBX - since we'd need the higher word to be clear in any case.
+    xor ecx, ecx
+    xor ebx, ebx
+    
+    mov eax, 0x8A
+    int 0x15
+    jc .88
+
+    mov eax, ebx
+    ; Now, that should move the lower word into the higher word.
+    shl ecx, 16
+    ; And put that into EAX.
+    or eax, ecx
+
+    ; Since it is length in 1KiB block, multiply it by 1024 to get the length in bytes.
+    mov ecx, 1024
+    mul ecx
+
+    mov dword [di], 0x100000
+    mov dword [di + 8], eax
+    ; That's free RAM - and a usable entry.
+    mov dword [di + 16], 1
+    mov dword [di + 20], 1
+    add di, 24
+
+    ; Increase the "we have that many entries" count.
+    inc bp
+    mov [MMapHeader.Entries], bp
+
+    jmp .Clean
+
+.88:
+    clc
+
+    ; Clear out EAX - since we'd need the higher word to be clear in any case.
+    xor eax, eax
+    
+    mov ah, 0x88
+    int 0x15
+    jc .CMOS
+
+    ; Since it is length in 1KiB block, multiply it by 1024 to get the length in bytes.
+    mov ecx, 1024
+    mul ecx
+
+    mov dword [di], 0x100000
+    mov dword [di + 8], eax
+    ; That's free RAM - and a usable entry.
+    mov dword [di + 16], 1
+    mov dword [di + 20], 1
+    add di, 24
+
+    ; Increase the "we have that many entries" count.
+    inc bp
+    mov [MMapHeader.Entries], bp
+
+    jmp .Clean
+
+.CMOS:
+    clc
+
+    xor eax, eax
+
+    ; Tell them we are reading the register (contains higher memory)!
+    mov al, 0x31
+    out 0x70, al
+    in al, 0x71
+
+    ; Shift left eight bits, to get it into AH.
+    shl ax, 8
+
+    ; And now the lower memory.
+    mov al, 0x30
+    out 0x70, al
+    in al, 0x71
+
+    ; Since it is length in 1KiB block, multiply it by 1024 to get the length in bytes.
+    mov ecx, 1024
+    mul ecx
+
+    test eax, eax
+    jz .Manual
+
+    mov dword [di], 0x100000
+    mov dword [di + 8], eax
+    ; That's free RAM - and a usable entry.
+    mov dword [di + 16], 1
+    mov dword [di + 20], 1
+    add di, 24
+
+    ; Increase the "we have that many entries" count.
+    inc bp
+    mov [MMapHeader.Entries], bp
+
+    jmp .Clean
+
+; The manual probing method here.
+.Manual:
+    clc
+
+    jmp .Return
 
 .Clean:
     mov esi, MMap
@@ -70,15 +273,75 @@ MMapBuild:
     popad
     ret
 
+; Tries to use EAX=0x0000E8X1 method of generating a memory map
+; @di             Should point to the memory map area.
+; @bp             The number of entries already there.
+; @eax            EAX should either contain 0xE881 or 0xE801.
+;     @rc     
+;                 Returns with carry set if failed.
+E8X1:
+    pushad
+
+    xor ebx, ebx
+
+    xor ecx, ecx
+    xor edx, edx                      ; ECX, EDX would contain the configured memory - if that remains zero, use extended (EAX, EBX)
+    
+    int 0x15                          ; Try AX=0xE8X1 - whatever it was.
+    
+    jc .Error                         ; Try E801, let's see - perhaps it works.
+    jecxz .Cont                       ; If ECX was zero, correct result is already in EAX, EBX pair.
+
+    mov eax, ecx
+    mov ebx, edx                      ; Or put the correct result in EAX, EBX pair.
+    
+; Now the correct values are in the EAX, EBX pair.
+.Cont:
+    cmp eax, 0x3C00
+    ja .Error                         ; The maximum value should be till the memory hole - if not, then don't trust it.
+
+    mov dword [di + 0], 0x100000      ; This entry starts at 1MiB.
+   
+    mov ecx, 1024
+    mul ecx                           ; Multiply EAX by 1024 - EAX contains 1KiB blocks between 1MiB and 15MiB - so make it bytes.
+    mov dword [di + 8], eax
+    mov dword [di + 16], 1            ; Free RAM - type 1.
+    mov dword [di + 20], 1            ; And 'to be used'.
+    add di, 24                        ; Move to next entry.
+    inc bp
+
+    mov eax, ebx                      ; Get the extended memory in EAX.
+    mov ecx, 64 * 1024                ; And since it's in 64 KiB blocks, multiply it with 64 * 1024.
+    mul ecx    
+    
+    mov dword [di + 0], 0x1000000     ; From 16MiB - above.
+    mov dword [di + 8], eax
+    mov dword [di + 16], 1            ; Free RAM - type 1.
+    mov dword [di + 20], 1            ; And 'to be used'.
+    add di, 24                        ; Now let's move to the next entry (if any).
+    inc bp                            ; BP now contains the number of entries.
+
+.Finished:
+    mov [MMapHeader.Entries], bp      ; Store the entry count
+    popad
+    clc                               ; Everything was successful.
+    ret
+
+.Error:
+    popad
+    stc
+    ret    
+
+
 ; Tries to use EAX=0x0000E820 method of generating a memory map
 ; @di             Should point to the memory map area.
+; @bp             The number of entries already there.
 ;     @rc
 ;                 Returns with carry set if failed.
 E820:
     pushad
 
     xor ebx, ebx                      ; EBX must be 0 to start.
-    xor bp, bp                        ; Store the number of entries in EBP for the moment.
     xor ecx, ecx                      ; The size of the entry is returned in CL - zero out the other bits.
     
     mov edx, 0x0534D4150              ; Expects "SMAP" in edx
@@ -206,7 +469,6 @@ MMapSwap:
     mov [esi + 40], ebp
     mov [esi + 44], edi
 
-
 .Return:
     popad
     ret
@@ -219,18 +481,19 @@ MMapSort:
     mov bp, [MMapHeader.Entries]      ; The number of entries in the memory map.
 
 .Loop:
+    push esi
     mov word [Finalized], 0
     mov ecx, 1                        ; Now Finalized is the count after which all elements have been sorted.
                                       ; Start the inner loop, where we check each entry for swapping.
 .Loop2:
-    mov eax, [si]          
-    cmp eax, [si + 24]
+    mov eax, [si + 4]          
+    cmp eax, [si + 28]
 
     jb .FLoop2
     ja .Swap
 
-    mov eax, [si + 4]
-    cmp eax, [si + 28]
+    mov eax, [si]
+    cmp eax, [si + 24]
 
     jb .FLoop2
 
@@ -245,6 +508,7 @@ MMapSort:
     jb .Loop2
 
 .FLoop:
+    pop esi
     mov bp, [Finalized]               ; Now, Finalized would contain till where we have finalized - after which we don't need to sort.
     cmp word [Finalized], 0           ; If we've finalized till 0, excellent. No need to loop again.
     jne .Loop 
